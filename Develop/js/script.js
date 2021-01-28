97daecc67461906f578b5748d4b9ecb0b46098d4
Zarin Salim <zarin_96@hotmail.co.uk>
2021-01-28 19:16:33 +0000

    initial commit

---
+++ Develop/js/script.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+          
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
