af3c93b60e33554ca035c57f14292081c4f5d5c4
kate <katy.madden99@gmail.com>
2021-02-01 22:49:49 +0000

    Changed API, added movie lists and created random function for five genres

--- Develop/js/netflix.js
+++ Develop/js/netflix.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
 
     var userGenre = [];
-    var length = userGenre.length + 1;
+    var movies = {
+        "Action": ["Black Panther", "Mission: Impossible - Fallout", "Mad Max: Fury Road", "Spider-Man: Into the Spider-Verse", "Wonder Woman", "Dunkirk", "Baby Driver", "Thor: Ragnarok", "Star Wars: The Last Jedi", "Incredibles 2"],
+        "Classics": ["Stand By Me", "The Breakfast Club", "Shawshank Redemption", "Rebel Without a Cause", "Gone With the Wind", "The Wizard of Oz", "Psycho", "Grease", "Dirty Dancing", "Footloose"],
+        "Comedies": ["Superbad", " Can't Hardly Wait", "Cloudy with a Chance of Meatballs", "50 First Dates", "Easy A", "Hail Caesar", "The Other Guys", "Scott Pilgrim vs. the World", "Wine Country", "Get Him to the Greek"],
+        "Documentaries": ["Blue Planet", "The Last Dance", "My Octopus Teacher", "American Murder: The Family Next Door", "Tiger King", "Louis Theroux: Savile", "Unsolved Mysteries", "The Disappearance of Madeleine McCann", "The Confession Killer", "Jeffrey Epstein: Filthy Rich"],
+        "Dramas": ["The Dig", "365 Days", "Rebecca", "The Green Mile", "Warrior", "Ava", "Bird Box", "Falling Down", "Twilight", "The Hunger Games"],
+        "Horrors": ["Sinister", "Annabelle", "Insideous", "The Exorcist", "The Ring", "Silence of The Lambs", "Thirteen", "The Babadook", "Eden Lake", "Final Destination"],
+        "International": ["Parasite", "The Wailing", "Death Note", "Bleach", "Train to Busan", "Monsoon Wedding", "Woman at War", "The Salesman", "Ida", "Roma"],
+        "Musicals": ["Mamma Mia", "Les Miserablés", "Hairspray", "Hamilton", "Little Shop of Horrors", "The Sound of Music", "The Rocky Horror Picture Show", "Willy Wonka & the Chocolate Factor", "Cats", "Bohemian Rhapsody"],
+        "Romance": ["Blended", "Bridget Jones's Diary", "The Notebook", "Fifty Shades of Grey", "Five Feet Apart", "The Proposal", "Her", "Titanic", "Romeo and Juliet", "Crazy Rich Asians"]
+    };
+    var randomNum = Math.floor(Math.random() * 11);
 
     $("input[type=checkbox]").prop("checked", false); //forces checkboxes unchecked on page load
 
@@ -29,7 +40,7 @@ $(document).ready(function () {
     });
 
     $("#movie-search").on("click", function (event) {
-        debugger;
+        // debugger;
         event.preventDefault(); //prevents default button event      
 
         if ($(this).hasClass("return") == true) { // returns to search view
@@ -38,6 +49,7 @@ $(document).ready(function () {
             $("#search-desc").text("Search by Genre");
             $("#page-title").show();
             $("#page-desc").show();
+            $(".show").show();
         }
 
         else {
@@ -45,38 +57,61 @@ $(document).ready(function () {
             $(this).text("Return to Search");
             $("#page-title").hide();
             $("#page-desc").hide();
-            $("#content").hide();
+            $(".show").hide();
 
             $("#search-desc").text("Here's our recommendations...");
-            for (var j = 0; j < userGenre.length; j++) {
+
+            for (j = 0; j < userGenre.length; j++) {
                 var currentFilm = userGenre[j];
+                var selectFilm = movies[currentFilm][randomNum];
+                console.log(selectFilm);
+
                 //api info
-                var apiURL = "https://netflix-unofficial.p.rapidapi.com/api/search?genre=" + currentFilm;
-                const settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": apiURL,
-                    "method": "GET",
-                    "headers": {
-                        "x-rapidapi-key": "65462b90a2msha45c78653a0b714p1594f9jsnb99b8a6c5fd2",
-                        "x-rapidapi-host": "netflix-unofficial.p.rapidapi.com"
-                    }
-                };
-
-
+                var queryURL = "https://www.omdbapi.com/?t=" + selectFilm + "&apikey=trilogy";
                 //Calling api
-                $.ajax(settings).done(function (response) {
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
                     console.log(response);
+                    saveMovInfo(response);
                 });
-            }
+            };
+            x = 0;
         }
     });
+    var j;
+    var x = 1;
 
-    // $(".return").on("click", function (event) {
-    //     event.preventDefault(); //prevents default button event      
-    //     $(this).attr("id", "movie-search");
-    //     $(this).text("Search");
-    // });
+    function saveMovInfo(response) {
+        if (userGenre.length == 5) {
+            if (x <= 2) {
+            var colEl = $("<div class='col s12 m4' id='mov-" + x + "'></div>");
+            var imgEl = $("<img src='" + response.Poster + "'/>");
+            var movHEl = $("<p class='mov-header'>" + response.Title + "</p>");
+            var movDescEl = $("<p class='mov-desc'>" + response.Plot + "</p>");
+
+            $("#content").append(colEl);
+            $(colEl).append(imgEl);
+            $(colEl).append(movHEl);
+            $(colEl).append(movDescEl);
+            }
+            else if (x > 2) {
+                var colEl = $("<div class='col s12 m4' id='mov-" + x + "'></div>");
+                var imgEl = $("<img src='" + response.Poster + "'/>");
+                var movHEl = $("<p class='mov-header'>" + response.Title + "</p>");
+                var movDescEl = $("<p class='mov-desc'>" + response.Plot + "</p>");
+    
+                $("#content").append(colEl);
+                $(colEl).append(imgEl);
+                $(colEl).append(movHEl);
+                $(colEl).append(movDescEl);
+                }
+            x++;
+        }
+        }
+
+
 
     function preventFive() { //stops users being able to select more than 5 genres at a time
         if (userGenre.length == 5) {
