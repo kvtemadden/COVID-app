420a944eebe0ccd59cc0e762ece98b5a995a7edb
Matthew Jones <mejones92@gmail.com>
2021-02-02 19:06:48 +0000

    Updates added to script to include functions for local storage.

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -1,26 +1,48 @@
 $(document).ready(function () {
+  // Get local storage
+  $(function getLocal() {
+    let searchCity = localStorage.getItem("city");
+    searchHistory.unshift(searchCity);
+    covidLocal();
+  })
+
   // Grab search item and store it in a variable
   function userSearch() {
     let search = $("#enter-city").val().trim();
     return search
   }
 
+  // Shows modal when triggered
+  function showModal(param) {
+    $("#" + param + "").show();
+  }
+
+  // Hides modal when triggered 
+  function hideModal(param) {
+    $(".close-alert").click(function() {
+      $("#" + param + "").hide();
+    })  
+  }
+
   // On click event for submit button calls API
   $("#search-btn").click(function (e) {
       if ($("input").val() != "") {
-      e.preventDefault();
-      let searchItem = userSearch();
-        if (searchItem === searchHistory[0]) {
-          alert("Data for " + searchItem + " is already showing");
-          $("#enter-city").val("");
-          preventDefault();
-        }
+        e.preventDefault();
+        let searchItem = userSearch();
+        localStorage.setItem("city", searchItem);
+          if (searchItem === searchHistory[0]) {
+            showModal("modal-data");
+            hideModal("modal-data");
+            $("#enter-city").val("");
+            preventDefault();
+          }
         searchHistory.unshift(searchItem);
         covidLocal();
         $("#enter-city").val("");
       }
       else if ($("input").val() === "") {
-        alert("Please enter a city");
+        showModal("modal-entercity");
+        hideModal("modal-entercity");
         e.preventDefault();
       }
   })
@@ -29,75 +51,95 @@ $(document).ready(function () {
   var searchHistory = [];
 
   // Function to create url
-  function urlBuild() {
+  function urlLocal() {
+    const parameters = {
+      "date":"date",
+      "areaName":"areaName",
+      "newCasesBySpecimenDate":"newCasesBySpecimenDate",
+      "cumDeaths28DaysByPublishDate":"cumDeaths28DaysByPublishDate",
+      "cumCasesBySpecimenDate":"cumCasesBySpecimenDate"
+    }
     let city = searchHistory[0];
-    let url = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaName=' + city + '&structure={"date":"date","areaName":"areaName","newCasesBySpecimenDate":"newCasesBySpecimenDate","newDeaths28DaysByPublishDate":"newDeaths28DaysByPublishDate","cumCasesBySpecimenDate":"cumCasesBySpecimenDate"}'
+    let url = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=utla;areaName=' + 
+    city + '&structure=' + JSON.stringify(parameters);
     return url;
   }
-  
+
+  function urlNational() {
+    const parameters = {
+      "date":"date",
+      "hospitalCases":"hospitalCases",
+      "newAdmissions":"newAdmissions",
+      "covidOccupiedMVBeds":"covidOccupiedMVBeds",
+      "newCasesByPublishDate":"newCasesByPublishDate",
+      "cumCasesByPublishDate":"cumCasesByPublishDate",
+      "cumDeaths28DaysByPublishDate":"cumDeaths28DaysByPublishDate",
+      "covidOccupiedMVBeds":"covidOccupiedMVBeds"
+    }
+    let url = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure=' + 
+    JSON.stringify(parameters);
+    return url;
+  }
+
   // Function and AJAX to call API
   function covidLocal() {
-    let covidURL = urlBuild();
-    console.log(covidURL)
+    let covidURL = urlLocal();
+    console.log(covidURL);
     $.ajax({
       url: covidURL,
       type: "GET",      
       success: function (response) {
-        console.log(response);
+        // Retrieve JSON response
         let area = response.data[2].areaName;
         let newCases = response.data[2].newCasesBySpecimenDate;
+        let deaths = response.data[2].cumDeaths28DaysByPublishDate;
         let totalCases = response.data[2].cumCasesBySpecimenDate;
-        let deaths = response.data[2].newDeaths28DaysByPublishDate;
+        
+        // Dynamically add local stats
+        $("#results").html("");
+        $("#results").append("<div class='row' style='padding-bottom:15px; text-align:center'><h4>Displaying Figures for: " + area + "</h4><h5>Data last updated 48hrs ago</h5></div>");
+        $("#results").append("<div class='col s3 cases green darken-1 white-text z-depth-2' id='cases'>" + "New Cases: " + newCases + "</div>");
+        $("#results").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='total'>" + "Total Cases: " + totalCases + "</div>");
+        $("#results").append("<div class='col s3 deaths green darken-1 white-text offset-s1 z-depth-2' id='deaths'>" + "Total Deaths: " + deaths + "</div>");
 
-        $(".results").html("");
-
-        $(".results").append("<div class='row' style='padding-bottom:15px; text-align:center'><h4>Displaying Figures for: " + area + "</h4><h5>Data last updated 48hrs ago</h5></div>");
-
-        $(".results").append("<div class='col s3 cases green darken-1 white-text z-depth-2' id='cases'>Cases: </div>");
-        $("#cases").append(newCases);
-
-        $(".results").append("<div class='col s3 deaths green darken-1 white-text offset-s1 z-depth-2' id='deaths'>Deaths: </div>");
-        $("#deaths").append(deaths);
-
-        $(".results").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='total'>Total cases to date: </div>");
-        $("#total").append(totalCases); 
-
+        // Make second API call
         covidNational();
       }
     })
   }
 
   function covidNational() {
+    let covidURL = urlNational();
+    console.log(covidURL);
     $.ajax({
-      url: 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","hospitalCases":"hospitalCases","newAdmissions":"newAdmissions","covidOccupiedMVBeds":"covidOccupiedMVBeds","newCasesByPublishDate":"newCasesByPublishDate","cumCasesByPublishDate":"cumCasesByPublishDate","cumDeaths28DaysByPublishDate":"cumDeaths28DaysByPublishDate"}',
+      url: covidURL,
       type: "GET",      
-      success: function (resp) {
-        console.log(resp);
-        let casesNat = resp.data[3].newCasesByPublishDate;
-        let hospital = resp.data[3].hospitalCases;
-        let admissions = resp.data[3].newAdmissions;
+      success: function (response) {
+        // Retrieve JSON response
+        let casesNat = response.data[3].newCasesByPublishDate;
+        let totalCasesNat = response.data[3].cumCasesByPublishDate;
+        let deathsNat = response.data[2].cumDeaths28DaysByPublishDate;
+        let hospital = response.data[3].hospitalCases;
+        let admissions = response.data[3].newAdmissions;
+        let beds = response.data[3].covidOccupiedMVBeds;
 
-        $(".results").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='cases-national'>New cases national: </div>");
-        $("#cases-national").append(casesNat); 
-        $(".results").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='hospital'>New hospital cases: </div>");
-        $("#hospital").append(hospital); 
-        $(".results").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='admissions'>New hospital admissions: </div>");
-        $("#admissions").append(admissions);
+        // Dynamically add national stats
+        $("#results2").html("");
+        $("#results2").append("<div class='row' style='padding-bottom:15px; text-align:center'><h4>Comparison to National Figures</h4><h5>Data last updated 48hrs ago</h5></div>");
+        $("#results2").append("<div class='col s3 hospital green darken-1 white-text z-depth-2' id='cases-national'>" + "New Cases: " + casesNat + " </div>");
+        $("#results2").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='total-national'>" + "Total Cases: " + totalCasesNat + "</div>");
+        $("#results2").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='deaths-national'>" + "Total Deaths: " + deathsNat + "</div>");
+
+        // Dynamically add hospital stats
+        $("#results3").html("");
+        $("#results3").append("<div class='row' style='padding-bottom:15px; text-align:center'><h4>Hospital Data for England</h4><h5>Data last updated 48hrs ago</h5></div>");
+        $("#results3").append("<div class='col s3 hospital green darken-1 white-text z-depth-2' id='admissions'>" + "New Hospital Admissions: " + admissions + "</div>");
+        $("#results3").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='hospital'>" + "Current Hospital Cases: " + hospital + "</div>");
+        $("#results3").append("<div class='col s3 hospital green darken-1 white-text offset-s1 z-depth-2' id='beds'>" + "Patients on Ventilators: " + beds + "</div>");
       }
     })
   }
-
-
-
-
-
-
-
 })
-
-
-
-
 
 
 
